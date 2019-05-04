1c931340a75678db0bb16475423343f7efe23d10
moskovanton <48486058+moskovanton@users.noreply.github.com>
2019-05-04 12:28:49 +0300

    Update Person.java

--- src/ru/avalon/java/j20/labs/models/Person.java
+++ src/ru/avalon/java/j20/labs/models/Person.java
@@ -1,9 +1,11 @@
 package ru.avalon.java.j20.labs.models;
 
+import java.util.Objects;
+
 /**
  * Представление о человеке.
  */
-public class Person {
+public class Person implements Comparable<Person> {
     /**
      * Имя.
      */
@@ -42,4 +44,39 @@ public class Person {
     public String getSurname() {
         return surname;
     }
+    
+        @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.name);
+        hash = 83 * hash + Objects.hashCode(this.surname);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.surname, other.surname)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int compareTo(Person t) {
+       int r = this.name.compareTo(t.name);
+       if(r != 0) return r;
+       r = this.surname.compareTo(t.surname);
+       if(r != 0) return r;
+       return 0;
+    }   
 }
